16d8d8b6e048ce2395aa5fe5d5daf6356fc80bdb
Nathan Raine <nraine18@gmail.com>
2021-05-17 19:20:09 -0500

    Added react-router (#1)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,22 +6,32 @@ import {Friends} from './Friends';
 import {auth} from '../firebase';
 import {useAuthState} from 'react-firebase-hooks/auth';
 import * as React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+const Home: React.FunctionComponent = () => {
+  return (
+    <h3>HOME</h3>
+  );
+}
+
+const Trade: React.FunctionComponent = () => {
+  return (
+    <h3>TRADE</h3>
+  );
+}
 
 const ContentSelector:React.FunctionComponent = () => {
   const [user] = useAuthState(auth);
   if (!user) return (<h1>PLEASE SIGN IN</h1>);
-  switch (window.location.pathname) {
-    case '/':
-      return (<h3>HOME</h3>);
-    case '/friends':
-      return (<Friends/>);
-    case '/deck':
-      return (<Deck/>);
-    case '/trade':
-      return (<h3>TRADE</h3>);
-    default:
-      return (<h3>UNKOWN LOCATION</h3>)
-  }
+  return (
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route path='/friends' component={Friends} />
+      <Route path='/deck' component={Deck} />
+      <Route path='/trade' component={Trade} />
+      <Redirect to='/' />
+    </Switch>
+  );
 }
 
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import 'regenerator-runtime/runtime';
 import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );

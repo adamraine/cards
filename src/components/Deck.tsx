22621b25aeb9f2315b39e3c896ec7eccc64903e1
Adam Raine <ad.st.raine@gmail.com>
2021-06-04 00:10:38 -0400

    auth state

--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -3,12 +3,15 @@ import {auth, db} from '../firebase';
 import {Card} from './Card';
 import {CardCreator} from './CardCreator';
 import styles from './Deck.module.scss';
+import {useAuthState} from 'react-firebase-hooks/auth';
 import {useCollectionData} from 'react-firebase-hooks/firestore';
 
 export const Deck:React.FunctionComponent = () => {
   const cards = db.collection('cards');
-  if (!auth.currentUser) throw new Error('User must be logged in.');
-  const query = cards.where('uid', '==', auth.currentUser.uid).orderBy('createdAt').limit(25);
+  const [user] = useAuthState(auth);
+  if (!user) throw new Error('User must be logged in.');
+
+  const query = cards.where('uid', '==', user.uid).orderBy('createdAt').limit(25);
   const [userCards] = useCollectionData<App.Card>(query, {idField: 'id'});
 
   return (

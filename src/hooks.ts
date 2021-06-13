e2464e209e358b6776e672487ba74e164489e992
Adam Raine <ad.st.raine@gmail.com>
2021-06-13 11:50:49 -0400

    update hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -17,24 +17,17 @@ export function useFriendsList(auth:firebase.default.auth.Auth, db: firebase.def
 
 type WindowSize = {width?: number, height?: number};
 export function useWindowSize():WindowSize {
-  // Initialize state with undefined width/height so server and client renders match
-  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
   const [windowSize, setWindowSize] = useState<WindowSize>({});
   useEffect(() => {
-    // Handler to call on window resize
     function handleResize() {
-      // Set window width/height to state
       setWindowSize({
         width: window.innerWidth,
         height: window.innerHeight,
       });
     }
-    // Add event listener
-    window.addEventListener("resize", handleResize);
-    // Call handler right away so state gets updated with initial window size
+    window.addEventListener('resize', handleResize);
     handleResize();
-    // Remove event listener on cleanup
-    return () => window.removeEventListener("resize", handleResize);
-  }, []); // Empty array ensures that effect is only run on mount
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
   return windowSize;
 }

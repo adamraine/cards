d0034d7debf1ee919a85bc130bf141969ae93f2e
Adam Raine <ad.st.raine@gmail.com>
2021-06-13 00:32:01 -0400

    better manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const {GenerateSW} = require('workbox-webpack-plugin');
 
@@ -40,36 +39,23 @@ module.exports = {
       filename: 'index.html',
       inject: 'head',
     }),
-    new WebpackPwaManifest({
-      name: 'Card Trader',
-      short_name: 'Card Trader',
-      description: 'Interactive card trading app built with React.',
-      background_color: '#1b1b1b',
-      theme_color: '#2f2f2f',
-      icons: [
-        {
-          src: __dirname + '/public/logo.svg',
-          sizes: [96, 128, 192, 256, 384, 512],
-        }
-      ]
-    }),
     new FaviconsWebpackPlugin({
       logo: __dirname + '/public/logo.svg',
       inject: true,
-      favicons: {
+      favicons:{
+        name: 'Card Trader',
+        short_name: 'Card Trader',
+        description: 'Interactive card trading app built with React.',
+        background_color: '#1b1b1b',
+        theme_color: '#2f2f2f',
+        start_url: '/',
+        manifestRelativePaths: true,
         icons: {
-          android: false,
-          appleIcon: [
-            'apple-touch-icon.png',
-          ],
+          android: true,
+          appleIcon: true,
           appleStartup: false,
           coast: false,
-          favicons: [
-            'favicon-16x16.png',
-            'favicon-32x32.png',
-            'favicon-48x48.png',
-            'favicon.ico'
-          ],
+          favicons: true,
           firefox: false,
           windows: false,
           yandex: false,

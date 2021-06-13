586e58036a4bf6e66f6aff69f0b3d1e7989b28ec
Adam Raine <ad.st.raine@gmail.com>
2021-06-13 13:47:37 -0400

    add maskable icons

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,38 @@
+const {Compilation, sources} = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const {GenerateSW} = require('workbox-webpack-plugin');
+
+/** @typedef {import('webpack').Compiler} Compiler */
+class AddMaskableIcons {
+
+  /**
+   * @param {Compiler} compiler 
+   */
+  apply(compiler) {
+    compiler.hooks.thisCompilation.tap(AddMaskableIcons.name, compilation => {
+      compilation.hooks.processAssets.tap(
+        {
+          name: AddMaskableIcons.name,
+          stage: Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE,
+        },
+        () => {
+          for (const file of Object.keys(compilation.assets)) {
+            if (!/manifest/.test(file)) continue;
+            const contents = compilation.getAsset(file);
+            const json = JSON.parse(contents.source.source());
+            const maskables = json.icons.map(i => ({...i, purpose: 'maskable'}));
+            json.icons.push(...maskables);
+            compilation.updateAsset(
+              file,
+              new sources.RawSource(JSON.stringify(json)),
+            )
+          }
+        }
+      )
+    });
+  }
+}
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
@@ -63,6 +95,7 @@ module.exports = {
       }
     }),
     new GenerateSW(),
+    new AddMaskableIcons(),
   ],
   devServer: {
     historyApiFallback: true,

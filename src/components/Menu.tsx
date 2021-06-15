67e8f9b3df6f0b86869b5048a993f663817d0fbe
Nathan Raine <nraine18@gmail.com>
2021-06-15 16:15:56 -0500

    horizontal menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -11,17 +11,9 @@ export const MenuItem:React.FunctionComponent<{label: string, href: string}> = (
 };
 
 export const Menu:React.FunctionComponent<{children: React.ReactNode}> = (props) => {
-  const [open, setOpen] = React.useState(false);
-  React.useEffect(() => {
-    const listener = () => open && setOpen(false);
-    document.addEventListener('click', listener);
-    return () => document.removeEventListener('click', listener);
-  });
   const classes = [styles.Menu];
-  if (open) classes.push(styles.open);
   return (
     <div className={classes.join(' ')}>
-      <div className={styles.icon} onClick={() => setOpen(!open)}>☰</div>
       <div className={styles.items}>
         {props.children}
       </div>

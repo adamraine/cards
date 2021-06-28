d7b19d4e219b283f0c0ef22510b0146c7f91368e
Adam Raine <ad.st.raine@gmail.com>
2021-06-27 22:32:45 -0400

    add confirmation popup

---
+++ src/components/Confirmation.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import styles from './Confirmation.module.scss';
+
+export const Confirmation:React.FC<{
+  onConfirm: React.EventHandler<React.SyntheticEvent>;
+  onCancel: React.EventHandler<React.SyntheticEvent>;
+  dismiss: () => void;
+}> = (props) => {
+  return (
+    <div className={styles.Confirmation}>
+      <div className={styles.text}>{props.children}</div>
+      <button className={styles.confirm} onClick={e => {
+        props.onConfirm(e);
+        props.dismiss();
+      }}>Confirm</button>
+      <button className={styles.cancel} onClick={e => {
+        props.onCancel(e);
+        props.dismiss();
+      }}>Cancel</button>
+    </div>
+  );
+};
